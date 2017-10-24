70cc33bebf6c451d8593cc1a02f1a5119e8cb831
Mark Miller <mark.kilpatrick.miller@gmail.com>
2017-10-24 12:58:44 -0400

    setup LinkList to work with our graphcool DB

--- src/components/LinkList.js
+++ src/components/LinkList.js
@@ -1,20 +1,21 @@
 import React, { Component } from "react";
+import { graphql, gql } from "react-apollo";
 import Link from "./Link";
 
 class LinkList extends Component {
   render() {
-    const linksToRender = [
-      {
-        id: "1",
-        description: "The Coolest GraphQL Backend 😎",
-        url: "https://www.graph.cool"
-      },
-      {
-        id: "2",
-        description: "The Best GraphQL Client",
-        url: "http://dev.apollodata.com/"
-      }
-    ];
+    // 1
+    if (this.props.allLinksQuery && this.props.allLinksQuery.loading) {
+      return <div>Loading</div>;
+    }
+
+    // 2
+    if (this.props.allLinksQuery && this.props.allLinksQuery.error) {
+      return <div>Error</div>;
+    }
+
+    // 3
+    const linksToRender = this.props.allLinksQuery.allLinks;
 
     return (
       <div>{linksToRender.map(link => <Link key={link.id} link={link} />)}</div>
@@ -22,4 +23,18 @@ class LinkList extends Component {
   }
 }
 
-export default LinkList;
+// 1
+const ALL_LINKS_QUERY = gql`
+  # 2
+  query AllLinksQuery {
+    allLinks {
+      id
+      createdAt
+      url
+      description
+    }
+  }
+`;
+
+// 3
+export default graphql(ALL_LINKS_QUERY, { name: "allLinksQuery" })(LinkList);
