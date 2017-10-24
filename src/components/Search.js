4b6b076c5bb2a91e7a78649b851ed3b2928ca3b4
Mark Miller <mark.kilpatrick.miller@gmail.com>
2017-10-24 14:50:51 -0400

    implement search functionality

--- src/components/Search.js
+++ src/components/Search.js
@@ -27,8 +27,42 @@ class Search extends Component {
   }
 
   _executeSearch = async () => {
-    // ... you'll implement this in a bit
+    const { searchText } = this.state;
+    const result = await this.props.client.query({
+      query: ALL_LINKS_SEARCH_QUERY,
+      variables: { searchText }
+    });
+    const links = result.data.allLinks;
+    this.setState({ links });
   };
 }
+
+const ALL_LINKS_SEARCH_QUERY = gql`
+  query AllLinksSearchQuery($searchText: String!) {
+    allLinks(
+      filter: {
+        OR: [
+          { url_contains: $searchText }
+          { description_contains: $searchText }
+        ]
+      }
+    ) {
+      id
+      url
+      description
+      createdAt
+      postedBy {
+        id
+        name
+      }
+      votes {
+        id
+        user {
+          id
+        }
+      }
+    }
+  }
+`;
 
 export default withApollo(Search);
