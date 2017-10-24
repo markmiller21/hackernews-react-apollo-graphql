e79d1511bc2bc0958510fd246eff900a933990bc
Mark Miller <mark.kilpatrick.miller@gmail.com>
2017-10-24 14:42:24 -0400

    basic search routing and page

--- src/components/App.js
+++ src/components/App.js
@@ -5,6 +5,7 @@ import LinkList from "./LinkList";
 import Login from "./Login";
 import CreateLink from "./CreateLink";
 import Header from "./Header";
+import Search from "./Search";
 
 class App extends Component {
   render() {
@@ -13,6 +14,7 @@ class App extends Component {
         <Header />
         <div className="ph3 pv1 background-gray">
           <Switch>
+            <Route exact path="/search" component={Search} />
             <Route exact path="/login" component={Login} />
             <Route exact path="/" component={LinkList} />
             <Route exact path="/create" component={CreateLink} />
