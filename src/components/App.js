5b5c1ba520f218dc69675e21202b9baf0629d8ce
Mark Miller <mark.kilpatrick.miller@gmail.com>
2017-10-24 13:38:52 -0400

    setup login component and router

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,10 @@
 import React, { Component } from "react";
 import { Switch, Route } from "react-router-dom";
-import Header from "./Header";
+
 import LinkList from "./LinkList";
+import Login from "./Login";
 import CreateLink from "./CreateLink";
+import Header from "./Header";
 
 class App extends Component {
   render() {
@@ -11,6 +13,7 @@ class App extends Component {
         <Header />
         <div className="ph3 pv1 background-gray">
           <Switch>
+            <Route exact path="/login" component={Login} />
             <Route exact path="/" component={LinkList} />
             <Route exact path="/create" component={CreateLink} />
           </Switch>
